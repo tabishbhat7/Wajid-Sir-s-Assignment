c2520c2070de24885e965667d3517b3a3dbe00c8
Tabish Bhat <tabishbhat11415@gmail.com>
2024-12-21 18:49:05 +0530

    Add files via upload

---
+++ tupple operation.py
@@ -0,0 +1,14 @@
+tuple = (1,2,3,4,5)
+sum = 0
+largest = max(tuple)
+smallest = min(tuple)
+for item in tuple :
+    sum += item
+
+print(sum)
+print(largest)
+print(smallest)
+
+tuple2 = (4,5,6,7,8,)
+x = 7 in tuple2
+print(x)
